13a6c9aa7e4830e2297d0cf4379e859cc21fe5a1
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2023-02-27 17:22:02 -0300

    Update Semestre - Turma e Graduação.py

--- pages/Semestre - Turma e Graduação.py
+++ pages/Semestre - Turma e Graduação.py
@@ -8,9 +8,9 @@ import altair as alt
 PAGE_CONFIG = {"page_title": "Indicadores", "page_icon": ":globe_with_meridians:", "layout": "wide"}
 st.set_page_config(**PAGE_CONFIG)
 
-base = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/base/codperlet_graduação.xlsx')
-base_turma = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/pages/codperlet_turma.xlsx')
-codperlet = pd.read_excel('/content/drive/MyDrive/Relatorios_20231/base/codperlet.xlsx')
+base = pd.read_excel('/codperlet_graduação.xlsx')
+base_turma = pd.read_excel('/codperlet_turma.xlsx')
+codperlet = pd.read_excel('/codperlet.xlsx')
 #
 
 cursos_grad = ['Administração', 'Ciências Econômicas','Direito','Engenharia de Produção','Engenharia de Computação']
