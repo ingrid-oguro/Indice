02aaea33e6e8344ffa5cde8e722878114a4ab84f
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2023-02-28 09:45:23 -0300

    Update Período Acadêmico - Turma e Graduação.py

--- pages/Período Acadêmico - Turma e Graduação.py
+++ pages/Período Acadêmico - Turma e Graduação.py
@@ -8,9 +8,9 @@ import altair as alt
 PAGE_CONFIG = {"page_title": "Indicadores", "page_icon": ":globe_with_meridians:", "layout": "wide"}
 st.set_page_config(**PAGE_CONFIG)
 
-base = pd.read_excel('Indice/pages/periodo_graduação.xlsx')
-base_turma = pd.read_excel('Indice/pages/periodo_turma.xlsx')
-periodo = pd.read_excel('Indice/pages/periodo.xlsx')
+base = pd.read_excel('pages/periodo_graduação.xlsx')
+base_turma = pd.read_excel('pages/periodo_turma.xlsx')
+periodo = pd.read_excel('pages/periodo.xlsx')
 #
 
 cursos_grad = ['Administração', 'Ciências Econômicas','Direito','Engenharia de Produção','Engenharia de Computação']
